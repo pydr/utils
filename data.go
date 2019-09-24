d1d232dcabbdeb88ef8cb7a6b95afa58f335f0d3
jack <dev@linuxmail.org>
2019-09-24 14:04:08 +0800

    feat: 增加方法

--- data.go
+++ data.go
@@ -3,20 +3,19 @@ package utils
 import "reflect"
 
 // 判断obj是否在target中，target支持的类型arrary,slice,map
-func Contain(obj interface{}, target interface{}) bool {
-	targetValue := reflect.ValueOf(target)
-	switch reflect.TypeOf(target).Kind() {
+func Contains(collection interface{}, that interface{}) bool {
+	targetValue := reflect.ValueOf(collection)
+	switch reflect.TypeOf(collection).Kind() {
 	case reflect.Slice, reflect.Array:
 		for i := 0; i < targetValue.Len(); i++ {
-			if targetValue.Index(i).Interface() == obj {
+			if targetValue.Index(i).Interface() == that {
 				return true
 			}
 		}
 	case reflect.Map:
-		if targetValue.MapIndex(reflect.ValueOf(obj)).IsValid() {
+		if targetValue.MapIndex(reflect.ValueOf(that)).IsValid() {
 			return true
 		}
 	}
-
 	return false
 }
