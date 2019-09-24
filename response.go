19216f6915840a63b2ff6d8501c3e67e1ef78e83
jack <dev@linuxmail.org>
2019-09-24 14:21:54 +0800

    feat: 增加方法

---
+++ response.go
@@ -0,0 +1,39 @@
+package utils
+
+import "net/http"
+
+type (
+	Code struct {
+		Status     bool   `json:"status"`
+		Code       int    `json:"code"`
+		Message    string `json:"message"`
+		HttpStatus int    `json:"-"`
+		Err        error  `json:"-"`
+	}
+
+	Response struct {
+		*Code
+		Data interface{} `json:"data,omitempty"`
+	}
+)
+
+func MakeCode(httpCode, code int, status bool, msg string, err error) *Code {
+	return &Code{
+		Status:     status,
+		Code:       code,
+		Message:    msg,
+		HttpStatus: httpCode,
+		Err:        err,
+	}
+}
+
+func MakeResp(code *Code, data interface{}) *Response {
+	return &Response{
+		Code: code,
+		Data: data,
+	}
+}
+
+var (
+	Success = MakeCode(http.StatusOK, 0, true, "success", nil)
+)
