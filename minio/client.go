bcaf266aa8c0fb48da495078b71f97debab68a9c
jack <dev@linuxmail.org>
2020-05-13 18:56:39 +0800

    feat: 增加流上传

--- minio/client.go
+++ minio/client.go
@@ -1,6 +1,9 @@
 package minio
 
-import "github.com/minio/minio-go/v6"
+import (
+	"github.com/minio/minio-go/v6"
+	"io"
+)
 
 type Client struct {
 	client *minio.Client
@@ -28,6 +31,22 @@ func (c *Client) Upload(bucket, location, filename, filepath string) error {
 	}
 
 	_, err = c.client.FPutObject(bucket, filename, filepath, minio.PutObjectOptions{ContentType: "application/octet-stream"})
+	return err
+}
+
+func (c *Client) UploadSteam(bucket, location, filename string, file io.Reader, fileSize int64) error {
+	exists, err := c.client.BucketExists(bucket)
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		if err = c.client.MakeBucket(bucket, location); err != nil {
+			return err
+		}
+	}
+
+	_, err = c.client.PutObject(bucket, filename, file, fileSize, minio.PutObjectOptions{ContentType: "application/octet-stream"})
 	return err
 }
 
